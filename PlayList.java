d94ae2ea529209e3ab0dbe926e9317311bd94366
Shir Amouyal <154405205+ShirAmouyal@users.noreply.github.com>
2024-02-09 13:18:15 +0200

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -165,7 +165,7 @@ class PlayList {
      *  If start is negative or greater than size - 1, returns -1.
      */
     private int minIndex(int start) {
-        if(start<0 || start>size-1)
+        if(start<0 || start>size)
         return -1;
         int min=300000;
         int minI=maxSize;
